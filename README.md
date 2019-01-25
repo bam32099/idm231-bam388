dcaf2d28ffe3b6f48242e997dbaa84672f89c369
Ben Murray <bam388@drexel.edu>
2019-01-24 19:11:33 -0500

    Images Update
    All Images to be used have been updated along with placeholder files.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # idm231_bam388
+
+**DISCLAIMER**
+This project is for educational purposes ONLY. In no way is this project for profit or an attempt to pass Pokemon as my own creation. Pokemon is owned by The Pokemon Company and Nintendo and I own nothing presented on this site including the Pokemon font and the Pokemon creatures shown.  
---
+++ srs/js.js
@@ -0,0 +1,42 @@
+// Variables
+
+function AstroSign(name, sound, image, desc) {
+    this.name = name;
+    this.sound = sound;
+    this.image = image;
+    this.desc = desc;
+    this.playSound = function() 
+}
+const selectedSign = 'taurus';
+console.log(eval(selectedSign).image);
+
+const taurus = new AstroSign('taurus', 'running_bull', 'image', 'desc');
+
+console.log(taurus.sound); running_bull
+taurus.playSound();
+
+if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
+    AstroSign = "Cap";
+  } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
+    AstroSign = "Sag";
+  } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
+    AstroSign = "Sco";
+  } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
+    AstroSign = "Lib";
+  } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
+    AstroSign = "Vir";
+  } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
+    AstroSign = "Leo";
+  } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
+    AstroSign = "Can";
+  } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
+    AstroSign = "Gem";
+  } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
+    AstroSign = "Tau";
+  } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
+    AstroSign = "Ari";
+  } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
+    AstroSign = "Pis";
+  } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
+    AstroSign = "Aqu";
+  }
